401a8848234292e3893613f390f45b6c9a21f85c
MoretonIsland <107469969+MoretonIsland@users.noreply.github.com>
2024-06-22 08:28:44 +0200

    Add files via upload

---
+++ showInsertedData.php
@@ -0,0 +1,33 @@
+<?php
+
+$csvFile = 'data.csv';
+$fileHandle = fopen($csvFile, 'r');
+if ($fileHandle === false) {
+    die(json_encode(['error' => "Nelze otevřít soubor: $csvFile"]));
+}
+
+// Přečtěte všechny řádky do pole
+$rows = [];
+while (($row = fgetcsv($fileHandle, 0, ';')) !== false) {
+    $rows[] = $row;
+}
+fclose($fileHandle);
+
+// Získání posledního řádku (poslední vložená data)
+$lastRow = end($rows);
+
+if ($lastRow === false) {
+    echo json_encode(['error' => 'Žádná data nebyla nalezena']);
+} else {
+    echo json_encode([
+        'DateTIme' => $lastRow[0],
+        'High temperature' => $lastRow[1],
+        'Low temperature' => $lastRow[2],
+        'Normal_high_temp' => $lastRow[3],
+        'Normal_low_temp' => $lastRow[4]
+    ]);
+}
+
+?>
+
+
